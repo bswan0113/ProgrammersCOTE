2b235677143aa060cf1e59aac6e9e3c34315fb00
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2026-03-09 17:48:52 +0900

    sort1 - k번째수

--- ConsoleApp2/ConsoleApp2/Sort/KstNum.cs
+++ ConsoleApp2/ConsoleApp2/Sort/KstNum.cs
@@ -1,8 +1,19 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int[] solution(int[] array, int[,] commands) {
-        int[] answer = new int[] {};
+        int[] answer = new int[commands.GetLength(0)];
+
+        for (int i = 0; i < commands.GetLength(0); i++)
+        {
+            var start = commands[i, 0] - 1;
+            var end = commands[i, 1] - 1;
+            var target = commands[i, 2] - 1;
+            var length = end - start + 1;
+            var newArr = array.Skip(start).Take(length).OrderBy(x => x).ToArray();
+            answer[i] = newArr[target];
+        }
         return answer;
     }
 }
