0ff50ae8684a77344908cb1ee1d172f73e80608e
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2026-03-09 21:10:32 +0900

    es2 - 모의고사

--- ConsoleApp2/ConsoleApp2/ExhaustiveSearch/SmallestRectangle.cs
+++ ConsoleApp2/ConsoleApp2/ExhaustiveSearch/SmallestRectangle.cs
@@ -1,21 +1,21 @@
-using System;
-
-public class Solution {
-    public int solution(int[,] sizes) {
-        int answer = 0;
-        int maxW = 0;
-        int maxH = 0;
-
-        for (int i = 0; i < sizes.GetLength(0); i++)
-        {
-            var wGreaterThanH = sizes[i, 0] > sizes[i, 1];
-            var targetW = wGreaterThanH ? sizes[i, 0] : sizes[i, 1];
-            var targetH = wGreaterThanH ? sizes[i, 1] : sizes[i, 0];
-            maxW = Math.Max(maxW, targetW);
-            maxH = Math.Max(maxH, targetH);
-        }
-
-
-        return maxW * maxH;
-    }
-}
+// using System;
+//
+// public class Solution {
+//     public int solution(int[,] sizes) {
+//         int answer = 0;
+//         int maxW = 0;
+//         int maxH = 0;
+//
+//         for (int i = 0; i < sizes.GetLength(0); i++)
+//         {
+//             var wGreaterThanH = sizes[i, 0] > sizes[i, 1];
+//             var targetW = wGreaterThanH ? sizes[i, 0] : sizes[i, 1];
+//             var targetH = wGreaterThanH ? sizes[i, 1] : sizes[i, 0];
+//             maxW = Math.Max(maxW, targetW);
+//             maxH = Math.Max(maxH, targetH);
+//         }
+//
+//
+//         return maxW * maxH;
+//     }
+// }
