41ffd24e514308a4713d1e3a2e68bf5a3e33176e
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2026-03-09 23:36:51 +0900

    es5 - 피로도

--- ConsoleApp2/ConsoleApp2/ExhaustiveSearch/Carpet.cs
+++ ConsoleApp2/ConsoleApp2/ExhaustiveSearch/Carpet.cs
@@ -1,30 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-public class Solution {
-    public int[] solution(int brown, int yellow)
-    {
-        int[] answer = new int[2];
-        HashSet<(int w ,int h)> numberSet = new HashSet<(int, int)>();
-        for (int i = 1; i <= Math.Sqrt(yellow); i++)
-        {
-            if (yellow % i == 0)
-            {
-                numberSet.Add((i, yellow / i));
-            }
-        }
-        for (int i = 0; i < numberSet.Count; i++)
-        {
-            var number = numberSet.ElementAt(i);
-            var result = (number.w + number.h + 2) * 2;
-            if (result == brown)
-            {
-                answer[0] = number.w > number.h ? number.w + 2: number.h + 2;
-                answer[1] = number.w > number.h ? number.h + 2: number.w + 2;
-            }
-        }
-        return answer;
-    }
-
-}
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+//
+// public class Solution {
+//     public int[] solution(int brown, int yellow)
+//     {
+//         int[] answer = new int[2];
+//         HashSet<(int w ,int h)> numberSet = new HashSet<(int, int)>();
+//         for (int i = 1; i <= Math.Sqrt(yellow); i++)
+//         {
+//             if (yellow % i == 0)
+//             {
+//                 numberSet.Add((i, yellow / i));
+//             }
+//         }
+//         for (int i = 0; i < numberSet.Count; i++)
+//         {
+//             var number = numberSet.ElementAt(i);
+//             var result = (number.w + number.h + 2) * 2;
+//             if (result == brown)
+//             {
+//                 answer[0] = number.w > number.h ? number.w + 2: number.h + 2;
+//                 answer[1] = number.w > number.h ? number.h + 2: number.w + 2;
+//             }
+//         }
+//         return answer;
+//     }
+//
+// }
--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,4 +1,3 @@
 ﻿
 Solution solution = new Solution();
 
-solution.solution(10, 24);
