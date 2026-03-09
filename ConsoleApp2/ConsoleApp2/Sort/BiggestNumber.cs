7289d446da33c07f85f0852c3de7f3620538a23a
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2026-03-09 20:11:34 +0900

    sort3 - hindex

--- ConsoleApp2/ConsoleApp2/Sort/BiggestNumber.cs
+++ ConsoleApp2/ConsoleApp2/Sort/BiggestNumber.cs
@@ -1,48 +1,48 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Text;
-
-public class Solution {
-    public string solution(int[] numbers) {
-        StringBuilder answer = new StringBuilder();
-        Dictionary<int, List<int>> numberDict = new Dictionary<int, List<int>>(9);
-
-        foreach (var num in numbers)
-        {
-            int firstDigit = num.ToString()[0] - '0';
-            if (!numberDict.ContainsKey(firstDigit))
-            {
-                numberDict.Add(firstDigit, new List<int>());
-            }
-            numberDict[firstDigit].Add(num);
-        }
-
-        foreach (var kvp in numberDict)
-        {
-            kvp.Value.Sort((x, y) =>
-            {
-                string sx = x.ToString();
-                string sy = y.ToString();
-                return String.Compare((sy + sx), sx + sy, StringComparison.Ordinal);
-            });
-        }
-
-        for (int i = 9; i >= 0; i--)
-        {
-            numberDict.TryGetValue(i, out var queue);
-            if(queue == null) continue;
-            foreach (var num in queue)
-            {
-                answer.Append(num);
-            }
-        }
-
-        if (answer.Length > 0 && answer[0] == '0')
-        {
-            return "0";
-        }
-
-        return answer.ToString();
-    }
-}
+// using System;
+// using System.Linq;
+// using System.Collections.Generic;
+// using System.Text;
+//
+// public class Solution {
+//     public string solution(int[] numbers) {
+//         StringBuilder answer = new StringBuilder();
+//         Dictionary<int, List<int>> numberDict = new Dictionary<int, List<int>>(9);
+//
+//         foreach (var num in numbers)
+//         {
+//             int firstDigit = num.ToString()[0] - '0';
+//             if (!numberDict.ContainsKey(firstDigit))
+//             {
+//                 numberDict.Add(firstDigit, new List<int>());
+//             }
+//             numberDict[firstDigit].Add(num);
+//         }
+//
+//         foreach (var kvp in numberDict)
+//         {
+//             kvp.Value.Sort((x, y) =>
+//             {
+//                 string sx = x.ToString();
+//                 string sy = y.ToString();
+//                 return String.Compare((sy + sx), sx + sy, StringComparison.Ordinal);
+//             });
+//         }
+//
+//         for (int i = 9; i >= 0; i--)
+//         {
+//             numberDict.TryGetValue(i, out var queue);
+//             if(queue == null) continue;
+//             foreach (var num in queue)
+//             {
+//                 answer.Append(num);
+//             }
+//         }
+//
+//         if (answer.Length > 0 && answer[0] == '0')
+//         {
+//             return "0";
+//         }
+//
+//         return answer.ToString();
+//     }
+// }
