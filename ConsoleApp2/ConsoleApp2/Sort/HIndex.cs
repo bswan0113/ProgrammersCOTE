8159c91c4e9d2aae9b18347300b9612d7de57d10
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2026-03-09 20:49:09 +0900

    es1 - 최소직사각형

--- ConsoleApp2/ConsoleApp2/Sort/HIndex.cs
+++ ConsoleApp2/ConsoleApp2/Sort/HIndex.cs
@@ -1,21 +1,21 @@
-using System;
-using System.Linq;
-public class Solution {
-    public int solution(int[] citations) {
-        int answer = 0;
-        citations = citations.OrderByDescending(x => x).ToArray();
-        for (int i = 0; i < citations.Length; i++)
-        {
-            if (citations[i] >= i + 1)
-            {
-                answer = i + 1;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return answer;
-    }
-}
+// using System;
+// using System.Linq;
+// public class Solution {
+//     public int solution(int[] citations) {
+//         int answer = 0;
+//         citations = citations.OrderByDescending(x => x).ToArray();
+//         for (int i = 0; i < citations.Length; i++)
+//         {
+//             if (citations[i] >= i + 1)
+//             {
+//                 answer = i + 1;
+//             }
+//             else
+//             {
+//                 break;
+//             }
+//         }
+//
+//         return answer;
+//     }
+// }
