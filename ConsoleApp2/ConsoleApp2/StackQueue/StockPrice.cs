1c830d1ed39c1d6817db20ed58067026579d043d
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2026-03-09 14:48:37 +0900

    sq-6 주식가격

---
+++ ConsoleApp2/ConsoleApp2/StackQueue/StockPrice.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.Length];
+
+        for (int i = 0; i < prices.Length; i++) {
+            int count = 0;
+            for (int j = i + 1; j < prices.Length; j++) {
+                count++;
+
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }
+            answer[i] = count;
+        }
+        return answer;
+    }
+}
