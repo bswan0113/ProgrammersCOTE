29b14116f2eada56c6b41ed9714d6dc847bb955b
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2026-03-10 18:25:00 +0900

    gr3 - 큰수만들기

--- ConsoleApp2/ConsoleApp2/Greedy/JoyStick.cs
+++ ConsoleApp2/ConsoleApp2/Greedy/JoyStick.cs
@@ -1,44 +1,44 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-public class Solution {
-    public int solution(string name) {
-        int answer = 0;
-        answer += GetWMove(name);
-        for (int i = 0; i < name.Length; i++)
-        {
-            var target = name[i];
-            answer += GetHMove(target);
-        }
-
-        return answer;
-    }
-
-    int GetHMove(char target)
-    {
-        int up = target - 'A';
-        int down = 'Z' - target + 1;
-        int minMove = Math.Min(up, down);
-        return minMove;
-    }
-
-    int GetWMove(string name)
-    {
-        int n = name.Length;
-        int minMove = n - 1;
-        for (int i = 0; i < n; i++)
-        {
-            int next = i + 1;
-            while (next < n && name[next] == 'A')
-            {
-                next++;
-            }
-            int move = i + i + (n - next);
-            minMove = Math.Min(minMove, move);
-            int moveBack = (n - next) * 2 + i;
-            minMove = Math.Min(minMove, moveBack);
-        }
-        return minMove;
-    }
-}
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+//
+// public class Solution {
+//     public int solution(string name) {
+//         int answer = 0;
+//         answer += GetWMove(name);
+//         for (int i = 0; i < name.Length; i++)
+//         {
+//             var target = name[i];
+//             answer += GetHMove(target);
+//         }
+//
+//         return answer;
+//     }
+//
+//     int GetHMove(char target)
+//     {
+//         int up = target - 'A';
+//         int down = 'Z' - target + 1;
+//         int minMove = Math.Min(up, down);
+//         return minMove;
+//     }
+//
+//     int GetWMove(string name)
+//     {
+//         int n = name.Length;
+//         int minMove = n - 1;
+//         for (int i = 0; i < n; i++)
+//         {
+//             int next = i + 1;
+//             while (next < n && name[next] == 'A')
+//             {
+//                 next++;
+//             }
+//             int move = i + i + (n - next);
+//             minMove = Math.Min(minMove, move);
+//             int moveBack = (n - next) * 2 + i;
+//             minMove = Math.Min(minMove, moveBack);
+//         }
+//         return minMove;
+//     }
+// }
---
+++ ConsoleApp2/ConsoleApp2/Greedy/MakeBigNumber.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public string solution(string number, int k) {
+        Stack<char> stack = new Stack<char>();
+
+        for (int i = 0; i < number.Length; i++)
+        {
+            char currentNum = number[i];
+            while (k > 0 && stack.Count > 0 && stack.Peek() < currentNum)
+            {
+                stack.Pop();
+                k--;
+            }
+            stack.Push(currentNum);
+        }
+
+        string answer = string.Concat(stack.Reverse());
+
+        if (k > 0)
+        {
+            answer = answer.Substring(0, answer.Length - k);
+        }
+
+        return answer;
+    }
+}
