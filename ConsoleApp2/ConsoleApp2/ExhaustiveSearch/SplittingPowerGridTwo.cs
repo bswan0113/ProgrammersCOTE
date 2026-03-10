51ffad160503e483632d1a561a760a5db2987ae0
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2026-03-10 16:51:12 +0900

    es7 - 모음사전

--- ConsoleApp2/ConsoleApp2/ExhaustiveSearch/SplittingPowerGridTwo.cs
+++ ConsoleApp2/ConsoleApp2/ExhaustiveSearch/SplittingPowerGridTwo.cs
@@ -1,43 +1,43 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-public class Solution {
-    public int solution(int n, int[,] wires) {
-        int answer = Int32.MaxValue;
-        for (int i = 0; i < wires.GetLength(0); i++)
-        {
-            var leftResult = LinkedWireCount(wires[i, 0],i, new List<int>(), wires);
-            var rightResult = n - leftResult;
-            var gap = Math.Abs(leftResult - rightResult);
-            answer = Math.Min(answer, gap);
-
-        }
-        return answer;
-    }
-
-    public int LinkedWireCount(int startPoint, int cutPoint, List<int> visitedPoint, int[,] wires)
-    {
-        visitedPoint.Add(startPoint);
-
-        for (int i = 0; i < wires.GetLength(0); i++)
-        {
-            if (i == cutPoint) continue;
-
-            int lPoint = wires[i, 0];
-            int rPoint = wires[i, 1];
-
-            if (lPoint == startPoint || rPoint == startPoint)
-            {
-                int nextPoint = (lPoint == startPoint) ? rPoint : lPoint;
-
-                if (!visitedPoint.Contains(nextPoint))
-                {
-                    LinkedWireCount(nextPoint, cutPoint, visitedPoint, wires);
-                }
-            }
-        }
-
-        return visitedPoint.Count;
-    }
-}
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+//
+// public class Solution {
+//     public int solution(int n, int[,] wires) {
+//         int answer = Int32.MaxValue;
+//         for (int i = 0; i < wires.GetLength(0); i++)
+//         {
+//             var leftResult = LinkedWireCount(wires[i, 0],i, new List<int>(), wires);
+//             var rightResult = n - leftResult;
+//             var gap = Math.Abs(leftResult - rightResult);
+//             answer = Math.Min(answer, gap);
+//
+//         }
+//         return answer;
+//     }
+//
+//     public int LinkedWireCount(int startPoint, int cutPoint, List<int> visitedPoint, int[,] wires)
+//     {
+//         visitedPoint.Add(startPoint);
+//
+//         for (int i = 0; i < wires.GetLength(0); i++)
+//         {
+//             if (i == cutPoint) continue;
+//
+//             int lPoint = wires[i, 0];
+//             int rPoint = wires[i, 1];
+//
+//             if (lPoint == startPoint || rPoint == startPoint)
+//             {
+//                 int nextPoint = (lPoint == startPoint) ? rPoint : lPoint;
+//
+//                 if (!visitedPoint.Contains(nextPoint))
+//                 {
+//                     LinkedWireCount(nextPoint, cutPoint, visitedPoint, wires);
+//                 }
+//             }
+//         }
+//
+//         return visitedPoint.Count;
+//     }
+// }
---
+++ ConsoleApp2/ConsoleApp2/ExhaustiveSearch/VowelDictionary.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int solution(string word) {
+        VDictionary dictionary = new VDictionary();
+        return dictionary.GetWordIndex(word);
+    }
+
+    class VDictionary
+    {
+        List<String> words = new List<String>();
+        private Char[] vowels = new[] {'A', 'E', 'I', 'O', 'U'};
+
+        private const int MaxWordLength = 5;
+
+        public VDictionary()
+        {
+            words.Add("A");
+            var canCreate = true;
+            for (int i = 0; canCreate; i++)
+            {
+                string prevWord = words[i];
+                string nextWord = GetNextWord(prevWord);
+                words.Add(nextWord);
+                if (nextWord == "UUUUU")
+                {
+                    canCreate = false;
+                }
+            }
+        }
+
+        public int GetWordIndex(string word)
+        {
+            if (!words.Contains(word)) return -1;
+            return words.IndexOf(word) + 1;
+        }
+
+        public string GetNextWord(string currentWord)
+        {
+            if (CanIncreaseLength(currentWord))
+            {
+                return currentWord + vowels[0];
+            }
+            else
+            {
+                for (int i = currentWord.Length - 1; i >= 0; i--)
+                {
+                    var targetChar = currentWord[i];
+                    if (CanIncreaseIndex(targetChar))
+                    {
+                        var nextChar = GetNextChar(targetChar);
+                        return currentWord.Substring(0, i) + nextChar;
+                    }
+                }
+            }
+
+            return "";
+        }
+
+        public int GetIndex(char c)
+        {
+            return Array.IndexOf(vowels, c);
+        }
+
+        public char GetNextChar(char c)
+        {
+            return vowels[GetIndex(c) + 1];
+        }
+
+        public bool CanIncreaseIndex(char c)
+        {
+            return c != vowels[vowels.Length - 1];
+        }
+
+        public bool CanIncreaseLength(string currentWord)
+        {
+            return currentWord.Length < MaxWordLength;
+        }
+    }
+}
